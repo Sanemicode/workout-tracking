b2570055fedfb0ba63d58d97ae6f975d5535b6a2
Lyubomyr <146125092+Sanemicode@users.noreply.github.com>
2026-09-21 14:13:32 +0300

    Updated database.py x3

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Обов'язково замініть 'ваш_пароль' на той, що ви придумали при встановленні PostgreSQL
 SQLALCHEMY_DATABASE_URL = os.getenv(
-    "https://dashboard.render.com/d/dpg-daogoaid0e5s7387656g-a?utm_source=gemini", 
+    "postgresql://workout_db_qjw8_user:Yx4pWEsUyCslWaGwhWpzqgt4QWlOoVHQ@dpg-daogoaid0e5s7387656g-a/workout_db_qjw8", 
     "postgresql://postgres:SilverSmith@localhost/workout_db"
 )
 
