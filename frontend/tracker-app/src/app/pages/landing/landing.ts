da7de7ed162b4e92fceb21302d64ae907088f1d9
Lyubomyr <146125092+Sanemicode@users.noreply.github.com>
2026-09-21 12:44:01 +0300

    Fix: hide auth links for guests and add landing translations

--- frontend/tracker-app/src/app/pages/landing/landing.ts
+++ frontend/tracker-app/src/app/pages/landing/landing.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common'; // Обов'язково для *ngIf
 import { AuthService } from '../../services/auth';
+import { TranslateModule } from '@ngx-translate/core'; // 1. Додаємо імпорт перекладу
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterModule, CommonModule], // Додано сюди
+  imports: [RouterModule, CommonModule, TranslateModule], // Додано сюди
   templateUrl: './landing.html',
   styleUrl: './landing.css'
 })
